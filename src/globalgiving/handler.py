83008462f25243d8014201eff68cc36d3bd862a6
Mindo <msilalahi@beachbody.com>
2017-11-27 14:18:48 -0800

    Added log

--- src/globalgiving/handler.py
+++ src/globalgiving/handler.py
@@ -6,14 +6,21 @@ Author: Mindo, Aaron, Kavita, David
 # -*- coding: utf-8 -*-
 
 import os
+import logging
 # from twilio.rest import Client
 
 twilio_sid_register = os.environ['twilio_sid_register']
 twilio_sid_update = os.environ['twilio_sid_update']
-twilio_auth_token = os.environ['twilio_auth_token']
+twilio_auth_token_register = os.environ['twilio_auth_token_register']
+twilio_auth_token_update = os.environ['twilio_auth_token_update']
+
+logging.basicConfig()
+log = logging.getLogger('logger')
+log.setLevel(logging.DEBUG)
 
 
 def post_globalgiving(event, context):
+
     handleEvent(event)
 
 
